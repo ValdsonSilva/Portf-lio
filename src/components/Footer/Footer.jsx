15ea6f23cb55d6ae97dbf9b0f1ca0f9d6937f5ac
Valdsonmacedo <Valdsonmacedo15@gmail.com>
2024-07-29 12:13:14 -0300

    feat: Acertando botão de voltar ao topo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
 
 function Footer() {
+
+    const [showButton, setShowButton] = useState(false)
+
+    const handleScroll = () => {
+        const firstSection = document.querySelector('#sobre_mim');
+        if (firstSection) {
+            const firsSectionBottom = firstSection.getBoundingClientRect().bottom;
+            setShowButton(firsSectionBottom <= 0);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
     return (
         <>
              <footer style={{
@@ -14,29 +33,31 @@ function Footer() {
             position: "relative"
         }}>
             <h1 style={{ color: "#000", fontSize: "1.5em"}}>&copy; 2024 Todos os direitos reservados.</h1>
-            <a href='#sobre_mim' style={{
-                position: "fixed",
-                bottom: "20px",
-                right: "20px",
-                // backgroundColor: "red"
-            }}>
-                <p style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    color: "#F57D1F",
-                    fontWeight: "400",
-                    fontSize: "30px",
-                    width: "60px",
-                    height: "60px",
-                    backgroundColor: "#fff",
-                    border: "2px solid #F57D1F",
-                    borderRadius: "50%",
-                    cursor: "pointer"
+            {showButton && (
+                <a href='#sobre_mim' style={{
+                    position: "fixed",
+                    bottom: "20px",
+                    right: "20px",
+                    // backgroundColor: "red"
                 }}>
-                    <FaArrowUp />
-                </p>
-            </a>
+                    <p style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        color: "#F57D1F",
+                        fontWeight: "400",
+                        fontSize: "30px",
+                        width: "60px",
+                        height: "60px",
+                        backgroundColor: "#fff",
+                        border: "2px solid #F57D1F",
+                        borderRadius: "50%",
+                        cursor: "pointer"
+                    }}>
+                        <FaArrowUp />
+                    </p>
+                </a>
+            )}
         </footer>
         </>
     )
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -286,7 +286,7 @@ function Main() {
                                 <h1 id="projetos_title" style={{fontSize: "3em", color: theme ? '#F57D1F' : '#F57D1F'}}>{repo.nome}</h1>
                                 <p style={{color: "#000", lineHeight: 1.5, textAlign: "justify"}}>
                                     {repo.alternativo}
-                                    <span style={{color: theme ? "#F57D1F" : "#19e2f1"}}>
+                                    <span style={{color: "#058ea9"}}>
                                         {repo.tecnologias.join(", ")}
                                     </span>
                                 </p>
